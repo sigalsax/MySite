a1ac11b988f2eef296287821e574870c99143a99
sigalsax <ssax18@gmail.com>
2018-01-11 08:30:24 -0500

    change of nomenclature

--- javascript.js
+++ javascript.js
@@ -1,30 +1,30 @@
 window.addEventListener('mouseover', function(e){
 	var proj = event.path[0].classList[1];
 	if (proj==='coarse-coding'){
-		document.getElementById('learn').style.color="#eaa835";
+		document.getElementById('learn-coarse').style.color="#eaa835";
 	}
 	else if (proj==='intra'){
-		document.getElementById('learn2').style.color="#eaa835";
+		document.getElementById('learn-intra').style.color="#eaa835";
 	}
 	else if (proj==='wanderer'){
-		document.getElementById('learn3').style.color="#eaa835";
+		document.getElementById('learn-wanderer').style.color="#eaa835";
 	}
 	else if (proj==='lifept'){
-		document.getElementById('learn4').style.color="#eaa835";
+		document.getElementById('learn-lifept').style.color="#eaa835";
 	}
 });
 window.addEventListener('mouseout', function(e){
 	var proj = event.path[0].classList[1];
 	if (proj==='coarse-coding'){
-		document.getElementById('learn').style.color="black";
+		document.getElementById('learn-coarse').style.color="black";
 	}
 	else if (proj==='intra'){
-		document.getElementById('learn2').style.color="black";
+		document.getElementById('learn-intra').style.color="black";
 	}
 	else if (proj==='wanderer'){
-		document.getElementById('learn3').style.color="black";
+		document.getElementById('learn-wanderer').style.color="black";
 	}
 	else if (proj==='lifept'){
-		document.getElementById('learn4').style.color="black";
+		document.getElementById('learn-lifept').style.color="black";
 	}
 });
