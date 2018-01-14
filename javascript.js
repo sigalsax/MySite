f467aa525acb9e07e2c740bc9956fa7ad035e3a4
sigalsax <ssax18@gmail.com>
2018-01-14 15:40:41 -0500

    adds sticky navbar js

--- javascript.js
+++ javascript.js
@@ -1,5 +1,31 @@
+// const nav=document.querySelector('#nav-bar');
+window.addEventListener('scroll', function(e){
+	const nav = document.querySelector('.nav-bar');
+
+	// grab top of nav bar
+	const topOfNavBar= nav.offsetTop+1;
+
+	// window.scrollY is how far we scrolled
+	console.log(topOfNavBar, window.scrollY);
+	if (window.scrollY >= topOfNavBar){
+		document.body.classList.add('fixed-nav');
+		document.body.style.paddingTop= nav.offsetHeight + 'px';
+		console.log(nav.offsetHeight);
+	}else{
+		document.body.style.paddingTop= 0;
+		document.body.classList.remove('fixed-nav');
+		
+
+	}
+});
+
+
 window.addEventListener('mouseover', function(e){
 	var proj = event.path[0].classList[1];
+	// console.log(event.path[0]);
+	// console.log("------")
+	// console.log(event.path[0].classList[0]);
+	// console.log(event.path[0].classList[1]);
 	if (proj==='coarse-coding'){
 		document.getElementById('learn-coarse').style.color="#eaa835";
 	}
